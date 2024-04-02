5f9021bb06c37655783b607ea064e498a58c20fc
sumitkumar33 <sumit.kumar@hestabit.in>
2024-04-02 20:08:34 +0530

    doctor experience attribute

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -3,12 +3,17 @@
 namespace App\Models;
 
 use App\Traits\HasTranslation;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Doctor extends Model
 {
     use HasFactory, HasTranslation;
+
+    protected $appends = [
+        'overall_experience',
+    ];
 
     protected $fillable = [
         'phone',
@@ -19,6 +24,20 @@ class Doctor extends Model
     public function experience()
     {
         return $this->hasMany(DoctorExp::class);
+    }
+
+    public function getOverallExperienceAttribute()
+    {
+        $experience = $this->experience->sortBy('from');
+        $totalExperience = [];
+        foreach ($experience as $exp) {
+            $expFrom = Carbon::parse($exp->from);
+            $expTo = Carbon::parse($exp->to);
+            for ($date = $expFrom; $date->lte($expTo); $date->addMonth()) {
+                $totalExperience[$date->format('F, Y')] = true;
+            }
+        }
+        return count($totalExperience);
     }
 
     public function hospitals()
